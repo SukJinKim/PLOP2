b87727eb94a5c131814d0f497b70ba349fda3848
SukJinKim <rkathrwks@gmail.com>
2019-12-11 02:10:07 +0900

    Complete AE

---
+++ src/main/java/edu/handong/csee/plt/Main.java
@@ -0,0 +1,82 @@
+package edu.handong.csee.plt;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import edu.handong.csee.plt.ast.AST;
+
+public class Main {
+	
+	private boolean onlyParser; // for -p option
+	private String code;
+	private String codeForParsing;
+	
+	public static void main(String[] args) {
+		Main main = new Main();
+		
+		main.run(args);
+	}
+	
+	private Options createOptions() {
+		Options options = new Options();
+		
+		options.addOption(Option.builder("p") //parse
+								.hasArg()
+								.build());
+		
+		return options;
+	}
+	
+	private boolean parseOptions(Options options, String[] args) {
+		CommandLineParser parser = new DefaultParser();
+		
+		CommandLine cmd;
+		try {
+			cmd = parser.parse(options, args);
+			
+			onlyParser = cmd.hasOption('p');
+			codeForParsing = cmd.getOptionValue('p'); //get the code for only parsing
+			
+		} catch (ParseException e) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private void run(String[] args){
+		Options options = createOptions();
+
+		// Parser
+		Parser parser = new Parser();
+		// interpreter
+		Interpreter interpreter = new Interpreter();
+		AST ast;
+
+		if(parseOptions(options, args)){
+			if(onlyParser) {
+				ast = parser.parse(codeForParsing);
+				
+				if(ast == null)
+					System.out.println("Syntax Error!");
+				
+				System.out.println(ast.getASTCode()); //print out the code parsed
+			}else {
+				code = args[0];
+				ast = parser.parse(code);
+				
+				if(ast == null)
+					System.out.println("Syntax Error!");
+				
+				String result = interpreter.interp(ast);
+				
+				System.out.println(result); //print out the result
+			}
+			
+		}
+	}		
+}
---
+++ src/main/java/edu/handong/csee/plt/ast/AST.java
@@ -0,0 +1,21 @@
+package edu.handong.csee.plt.ast;
+
+public class AST {
+	
+	public String getASTCode() {
+		String astCode="";
+		
+		if(this instanceof Add)
+			astCode = ((Add)this).getASTCode();
+		
+		if(this instanceof Num)
+			astCode = ((Num)this).getASTCode();
+		
+		//Sub
+		if(this instanceof Sub)
+			astCode = ((Sub)this).getASTCode();
+
+		return astCode;
+	}
+}
+
