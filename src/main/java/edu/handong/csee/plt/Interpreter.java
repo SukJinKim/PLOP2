b6aea9270f57e1ea378cc0ea96c247801d1d5861
SukJinKim <rkathrwks@gmail.com>
2019-12-18 21:42:53 +0900

    Add Laziness

--- src/main/java/edu/handong/csee/plt/Interpreter.java
+++ src/main/java/edu/handong/csee/plt/Interpreter.java
@@ -198,9 +198,6 @@ public class Interpreter {
 		}
 		
 		if(ast instanceof If0) {
-//			(if (numzero? (interp test-expr ds))
-//                (interp then-expr ds)
-//                (interp else-expr ds))
 			
 			If0 ifZero = (If0) ast;
 			
@@ -209,14 +206,6 @@ public class Interpreter {
 		}
 		
 		if(ast instanceof Rec) {
-//			[rec  (bound-id named-expr fst-call)
-//             (local [(define value-holder (box (numV 198)))
-//                         (define new-ds (aRecSub bound-id
-//                                                 value-holder
-//                                                 ds))]
-//               (begin
-//                             (set-box! value-holder (interp named-expr new-ds))
-//                             (interp fst-call new-ds)))]))
 			Rec rec = (Rec) ast;
 			char boundId = rec.getName();
 			AST namedExpr = rec.getNamedExpr();
@@ -239,12 +228,6 @@ public class Interpreter {
 	
 //	private static FAEValue strict(FAEValue v) throws FreeIdentifierException, BoxTypeException {
 //		if(v instanceof ExprV) {
-//			//[exprV (expr ds v-box)
-//             //(if (not (unbox v-box))
-//               //  (local [(define v (strict (interp expr ds)))]
-//                 //  (begin (set-box! v-box v)
-//                   //       v)) ;true
-//                 //(unbox v-box))] ;false
 //			
 //			boolean vBox = (boolean) ((ExprV) v).getVal().getBoxVal();
 //			AST expr = ((ExprV) v).getExpr();
@@ -282,7 +265,6 @@ public class Interpreter {
 		
 	}
 
-//	private static FAEValue lookUp(char name, DefrdSub ds) throws FreeIdentifierException, BoxTypeException{
 	private static int lookUp(char name, DefrdSub ds) throws FreeIdentifierException, BoxTypeException{
 		if(ds instanceof MtSub){
 			
@@ -292,12 +274,9 @@ public class Interpreter {
 		if(ds instanceof ASub) {
 			char i = ((ASub) ds).getName();
 			int addr = ((ASub)ds).getAddr();
-//			FAEValue v = ((ASub) ds).getValue();
 			DefrdSub saved = ((ASub) ds).getDs();
 			
 			return (name == i) ? addr : lookUp(name, saved); //[aSub (n addr d) (if(symbol=? n name) addr (lookup name d))]
-//			return (name == i) ? strict(v) : lookUp(name, saved); //[aSub (i v saved) (if (symbol=? i name) v (lookup name saved))]
-			
 		}
 		
 //		if(ds instanceof ARecSub) {	
@@ -315,11 +294,6 @@ public class Interpreter {
 		if(sto instanceof MtSto) {
 			throw new NoValueException("No Value at address"); //[mtSto () (error 'store-lookup "No value at address")]
 		}
-		
-//	    [aSto (location value rest-store)
-//        (if(= location address)
-//           value
-//           (store-lookup address rest-store))]
 		
 		ASto asto = (ASto)sto;
 		int location = asto.getAddr();
@@ -381,242 +355,4 @@ public class Interpreter {
 		
 		return numOp('-', lhs, rhs);
 	}
-	
-/**
- * 
- * @deprecated
- * 
- * 
-	public static FAEValue interp(AST ast, DefrdSub ds) throws FreeIdentifierException, BoxTypeException {
-		if(ast instanceof Num) {
-			
-			return new NumV(((Num)ast).getStrNum()); //[num    (n)      (numV n)]
-		}
-		
-		if(ast instanceof Add) {
-			Add add = (Add)ast;
-		
-			return numPlus(interp(add.getLhs(), ds), interp(add.getRhs(), ds)); //[add (l r) (num+ (interp l) (interp r))] 
-		}
-		
-		if(ast instanceof Sub) {
-			Sub sub = (Sub)ast;
-			
-			return numMinus(interp(sub.getLhs(), ds), interp(sub.getRhs(), ds)); //[sub    (l r)    (num- (interp l ds) (interp r ds))]
-		}
-		
-		if(ast instanceof Id) {
-			
-			return lookUp(((Id)ast).getId(), ds); //[id     (s)      (lookup s ds)]
-		}
-		
-		if(ast instanceof Fun) {
-			Fun fun = (Fun)ast;
-			char p = fun.getParam();
-			AST b = fun.getBody();
-			
-			return new ClosureV(p, b, ds); //[fun    (p b)    (closureV p b ds)]
-		}
-	
-		if(ast instanceof App) {
-//			[app  (f a) (local [(define ftn (interp f ds))]
-//                    (interp (closureV-body ftn)
-//                                 (aSub (closureV-param ftn)
-//                                             (interp a ds)
-//                                             (closureV-ds ftn))))]
-			
-			App app = (App) ast;
-			AST f = app.getFtn();
-			AST a = app.getArg();
-			
-			ClosureV ftn = (ClosureV) interp(f, ds);
-			return interp(ftn.getBody(), new ASub(ftn.getName(), interp(a, ds), ftn.getDs()));
-
-//			[app (f a)   (local [(define f-val (strict (interp f ds)))
-//		                          (define a-val (exprV a ds (box #f)))]
-//		                    (interp (closureV-body f-val)
-//		                            (aSub (closureV-param f-val)
-//		                                  a-val
-//		                                  (closureV-ds f-val))))]
-			
-			
-//			ClosureV fVal = (ClosureV)strict(interp(f, ds));
-//			FAEValue fVal = strict(interp(f, ds));
-//			ExprV aVal = new ExprV(a, ds, new Box<Boolean>(false));
-			
-//			return interp(fVal.getBody(), new ASub(fVal.getName(), aVal, fVal.getDs()));
-	
-		}
-		
-		if(ast instanceof If0) {
-//			(if (numzero? (interp test-expr ds))
-//                (interp then-expr ds)
-//                (interp else-expr ds))
-			
-			If0 ifZero = (If0) ast;
-			
-			return (isZero((NumV)interp(ifZero.getTestExpr(), ds))) ? interp(ifZero.getThenExpr(), ds) : interp(ifZero.getElseExpr(), ds);
-		}
-		
-		if(ast instanceof Rec) {
-//			[rec  (bound-id named-expr fst-call)
-//             (local [(define value-holder (box (numV 198)))
-//                         (define new-ds (aRecSub bound-id
-//                                                 value-holder
-//                                                 ds))]
-//               (begin
-//                             (set-box! value-holder (interp named-expr new-ds))
-//                             (interp fst-call new-ds)))]))
-			Rec rec = (Rec) ast;
-			char boundId = rec.getName();
-			AST namedExpr = rec.getNamedExpr();
-			AST fstCall = rec.getFstCall();
-		
-			Box<FAEValue> valueHolder = new Box<FAEValue>(new NumV("198"));
-			ARecSub newDS = new ARecSub(boundId, valueHolder, ds);
-	
-			valueHolder.setBoxVal(interp(namedExpr, newDS));
-			
-			return interp(fstCall, newDS);
-		}
-		
-		return null;
-		
-	}
-	*/
-
-	/**
-	 * @deprecated it's not used anymore
-	 * @param ast
-	 * @return
-	 * @throws FreeIdentifierException
-	 *
-	public AST interp(AST ast) throws FreeIdentifierException {
-		
-		if(ast instanceof Num) {
-			
-			return (Num)ast; //[num (n) fwae]
-		}
-		
-		if(ast instanceof Add) {
-			Add add = (Add)ast;
-		
-			return numPlus(interp(add.getLhs()), interp(add.getRhs())); //[add (l r) (num+ (interp l) (interp r))] 
-		}
-		
-		if(ast instanceof Sub) {
-			Sub sub = (Sub)ast;
-			
-			return numMinus(interp(sub.getLhs()), interp(sub.getRhs()));
-		}
-		
-		if(ast instanceof With) {
-			With with = (With)ast;
-			AST expr = with.getExpr();
-			char idtf = with.getIdtf();
-			AST val = with.getVal();
-			
-			return interp(subst(expr, 
-								idtf, 
-								interp(val)));
-		}
-		
-		if(ast instanceof Id) {
-			
-			throw new FreeIdentifierException("Free identifier"); //[id (s) (error 'interp "free identifier ~a" s)]
-		}
-		
-		if(ast instanceof Fun) { 
-			
-			return (Fun)ast; //[fun (p b) fwae]
-		}
-	
-		if(ast instanceof App) {
-			// [app (f a) (local [(define ftn (interp f))]
-            //(interp (subst (fun-body ftn) (fun-param ftn) (interp a))))]
-			
-			App app = (App) ast;
-			Fun f = (Fun) app.getFtn();
-			AST a = app.getArg();
-			
-			AST body = f.getBody();
-			char param = f.getParam();
-			
-			return interp(subst(body, param, interp(a)));
-		}
-		
-		return null;
-	}
-	*/
-	
-	/**
-	 * @deprecated it's not used anymore.
-	 * @param expr
-	 * @param idtf
-	 * @param val
-	 * @return
-	 *
-	private static AST subst(AST expr, char idtf, AST val) {
-		
-		if(expr instanceof Num) { //[num (n) wae]
-			return ((Num)expr); 
-		}
-		
-		if(expr instanceof Add) { //[add (l r) (add (subst l idtf val) (subst r idtf val))]
-			Add add = (Add)expr;
-			AST lhs = add.getLhs();
-			AST rhs = add.getRhs();
-			
-			return new Add(subst(lhs, idtf, val), subst(rhs, idtf, val));
-		}
-		
-		if(expr instanceof Sub) { //[sub (l r) (sub (subst l idtf val) (subst r idtf val))]
-			Sub sub = (Sub)expr;
-			AST lhs = sub.getLhs();
-			AST rhs = sub.getRhs();
-			
-			return new Sub(subst(lhs, idtf, val), subst(rhs, idtf, val));
-		}
-		
-		if(expr instanceof With) { //[with (i v e) (with i (subst v idtf val) (if (symbol=? i idtf) e (subst e idtf val)))] 
-								   // i, v, e from new one, idtf, val from old one
-			With with = (With)expr;
-			
-			char i = with.getIdtf();
-			AST v = with.getVal();
-			AST e = with.getExpr();
-			
-			return new With(i,
-							subst(v, idtf, val),
-							(i == idtf) ? e : subst(e, idtf, val));
-		}
-		
-		if(expr instanceof Id) { //[id (name) (cond [(equal? name idtf) val] [else exp])]
-			Id id = (Id)expr;
-			char s = id.getId();
-			
-			return (s == idtf) ? val : expr;
-		}
-		
-		if(expr instanceof App) { //[app (f arg) (app (subst f idtf val) (subst arg idtf val))]
-			App app = (App)expr;
-			
-			AST f = app.getFtn();
-			AST a = app.getArg();
-			
-			return new App(subst(f, idtf, val), 
-						subst(a, idtf, val));
-		}
-		
-		if(expr instanceof Fun) { //[fun (id body) (if (equal? idtf id) exp (fun id (subst body idtf val)))]
-			Fun fun = (Fun) expr;
-			
-			char id = fun.getParam();
-			AST body = fun.getBody();
-			
-			return (idtf == id) ? expr : new Fun(id, subst(body, idtf, val));
-		}
-		
-		return expr;
-	}*/
 }
